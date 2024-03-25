91ed4d495d4846d17a1b5fb935c6824f0e12cc9e
강민승 <k4minseung@gmail.com>
2024-03-25 23:17:35 +0900

    Feat : Cert Domain 추가

--- src/main/java/com/gathering/info/info/domain/certifications/Certifications.java
+++ src/main/java/com/gathering/info/info/domain/certifications/Certifications.java
@@ -1,4 +1,27 @@
 package com.gathering.info.info.domain.certifications;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder(toBuilder = true)
 public class Certifications {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long certificationId;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Column(nullable = false, length = 30)
+    private String provider;
+
+    private LocalDateTime acquisitedTime;
+
+    private LocalDateTime expiredTime;
 }
